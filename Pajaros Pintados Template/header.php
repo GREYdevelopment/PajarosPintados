bc08a70d9319d7160a656cf6baf808d186ca0fb4
GreyDevelopment <43412674+GREYdevelopment@users.noreply.github.com>
2018-11-06 19:18:31 -0300

    Add files via upload

--- Pajaros Pintados Template/header.php
+++ Pajaros Pintados Template/header.php
@@ -31,12 +31,27 @@ $('#nav-icon3').click(function(){
     $('#nav-icon3').toggleClass('open');
     $('#nav-icon3').toggleClass('menu-bg');
     $('.content-menu-desplegable').toggleClass( 'menu-animation');
+    $('.content-list-menu').toggleClass( 'list-general-animation');
+
+    $('.lista-actividades').removeClass( 'menu-animation-dropdown');
+    $('.lista-destinos').removeClass( 'menu-animation-dropdown-destinos');
 });
 
   $('.menu-text').click(function(){
     $('#nav-icon3').toggleClass('open');
     $('#nav-icon3').toggleClass('menu-bg');
     $('.content-menu-desplegable').toggleClass( 'menu-animation');
+    $('.content-list-menu').toggleClass( 'list-general-animation');
+});
+
+$('.button-dropdown-actividades').click(function(){
+    $('.lista-actividades').toggleClass( 'menu-animation-dropdown');
+    $('.lista-destinos').removeClass( 'menu-animation-dropdown-destinos');
+});
+
+$('.button-dropdown-destinos').click(function(){
+    $('.lista-destinos').toggleClass( 'menu-animation-dropdown-destinos');
+    $('.lista-actividades').removeClass( 'menu-animation-dropdown');
 });
 
 
@@ -75,8 +90,8 @@ $('#nav-icon3').click(function(){
       <ul class="content-list-menu">
         <li class="item-menu-one"><a href="">INICIO</a></li>
         <li class="item-menu-one"><a href="">MAPA DE VIAJE</a></li>
-        <li class="item-menu-two">ACTIVIDADES <span class="caret"></span></li>
-        <div class="content-ministerio content-list-desplegable">
+        <li class="item-menu-two button-dropdown-actividades">ACTIVIDADES <span class="caret"></span></li>
+        <div class="content-list-desplegable lista-actividades">
             <li><a target="_parent" href="/termas">Termas</a></li>
             <li><a target="_parent" href="/">Naturaleza y áreas protegidas</a></li>
             <li><a target="_parent" href="/">Aventura Náutica</a></li>
@@ -84,6 +99,23 @@ $('#nav-icon3').click(function(){
             <li><a target="_parent" href="/">Pueblos con encanto</a></li>
             <li><a target="_parent" href="/">Sabores</a></li>
         </div>
+
+        <li class="item-menu-two button-dropdown-destinos">DESTINOS <span class="caret"></span></li>
+        <div class="content-list-desplegable lista-destinos">
+            <li><a target="_parent" href="/termas">Termas</a></li>
+            <li><a target="_parent" href="/">Naturaleza y áreas protegidas</a></li>
+            <li><a target="_parent" href="/">Aventura Náutica</a></li>
+            <li><a target="_parent" href="/">Patrimonio y cultura</a></li>
+            <li><a target="_parent" href="/">Pueblos con encanto</a></li>
+            <li><a target="_parent" href="/">Sabores</a></li>
+            <li><a target="_parent" href="/termas">Termas</a></li>
+            <li><a target="_parent" href="/">Naturaleza y áreas protegidas</a></li>
+            <li><a target="_parent" href="/">Aventura Náutica</a></li>
+            <li><a target="_parent" href="/">Patrimonio y cultura</a></li>
+            <li><a target="_parent" href="/">Pueblos con encanto</a></li>
+            <li><a target="_parent" href="/">Sabores</a></li>
+        </div>
+
       </ul>
   </div>
 
